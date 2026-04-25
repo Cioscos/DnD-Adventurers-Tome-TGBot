dc59cc5440d056082331d7ff69bcf9cb20795fac
Cioscos <claudiocicca1998@gmail.com>
2026-04-25 19:12:38 +0200

    feat(dice-packs): add "poison_dice" to bundled packs

--- webapp/src/dice/packs/registry.ts
+++ webapp/src/dice/packs/registry.ts
@@ -1,5 +1,5 @@
 // webapp/src/dice/packs/registry.ts
-export const BUNDLED_PACKS = ['default', 'hell_dice'] as const
+export const BUNDLED_PACKS = ['default', 'hell_dice', 'poison_dice'] as const
 export type PackId = (typeof BUNDLED_PACKS)[number]
 
 export function isBundledPack(id: string): id is PackId {
